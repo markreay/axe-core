3deb5a1e2ec26c7313d8b5a1caca4cbb8ec50648
Jey <jey.nandakumar@gmail.com>
2018-08-20 12:54:37 +0100

    test: run all integration tests

--- build/test/config.js
+++ build/test/config.js
@@ -40,12 +40,8 @@ exports = module.exports = function(grunt, options) {
 				log: true,
 				urls: mapToUrl(
 					[
-						// 'test/integration/full/**/*.html',
-						'!test/integration/full/**/frames/**/*.html',
-						// ignore integration tests on cssom figuring out reason of failure in webdriver
-						'test/integration/full/css-orientation-lock/*.html',
-						'test/integration/full/preload/*.html',
-						'test/integration/full/preload-cssom/*.html'
+						'test/integration/full/**/*.html',
+						'!test/integration/full/**/frames/**/*.html'
 					],
 					'<%= connect.test.options.port %>'
 				),
