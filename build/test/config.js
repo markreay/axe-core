a8ce7e14d546fd94eace98a0ab3a943de53b5013
Jey <jey.nandakumar@gmail.com>
2018-08-20 12:38:43 +0100

    test: all preload and orientation tests

--- build/test/config.js
+++ build/test/config.js
@@ -43,9 +43,9 @@ exports = module.exports = function(grunt, options) {
 						// 'test/integration/full/**/*.html',
 						'!test/integration/full/**/frames/**/*.html',
 						// ignore integration tests on cssom figuring out reason of failure in webdriver
-						'test/integration/full/css-orientation-lock/*.html'
-						// 'test/integration/full/preload/*.html',
-						// 'test/integration/full/preload-cssom/*.html'
+						'test/integration/full/css-orientation-lock/*.html',
+						'test/integration/full/preload/*.html',
+						'test/integration/full/preload-cssom/*.html'
 					],
 					'<%= connect.test.options.port %>'
 				),
